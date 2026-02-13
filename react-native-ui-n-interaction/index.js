7d72f2d52b37b469acca4da78b9a19e544acd0f6
Cie <jocelynbendoy26@gmail.com>
2026-02-13 15:34:09 +0800

    Done with percentage dimensions section

--- react-native-ui-n-interaction/index.js
+++ react-native-ui-n-interaction/index.js
@@ -1,10 +1,9 @@
-import App from './App';
 import LotsOfStyles from './components/Styles.js';
 import FixedDimensionsBasics from './components/FixedDimensions.js';
 import FlexDimensionsBasics from './components/FlexDimensions.js';
-
+import PercentageDimensionsBasics from './components/PercentageDimensions.js';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(FlexDimensionsBasics);
+registerRootComponent(PercentageDimensionsBasics);

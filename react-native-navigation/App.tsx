361c11e6b247786baa0a6f0fd0b78255e44af871
Cie <jocelynbendoy26@gmail.com>
2026-04-20 12:15:07 +0800

    Done with installing the elements library

--- react-native-navigation/App.tsx
+++ react-native-navigation/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { createStaticNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button } from '@react-navigation/elements';
 
 function HomeScreen() {
   return (
